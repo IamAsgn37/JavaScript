6cdc94aa5b10f2bcca3fefd2fc0683c42ecf0dd0
Aashish Gulshan <78717816+IamAsgn37@users.noreply.github.com>
2023-06-23 09:58:56 +0000

    variables

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,7 @@
 console.log("Hey Aashish..!!");
+let accountId = "aashish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+
+console.log(accountCity);
+console.table([accountId, accountPassword, accountCity])
---
+++ basics/variable.js
@@ -0,0 +1,3 @@
+"use strict";  // treat all js code as newer version.
+
+// alert(5+5)  // It giving error because we are using node.js not Browser
